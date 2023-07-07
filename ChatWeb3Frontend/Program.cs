ba98612ff93542b2ff5f78af4154abc4d4b82ce0
Rakesh Kumar <rakesh.kumar23@chicmic.co.in>
2023-07-07 18:11:44 +0530

    Designed the UI of Login Page

--- ChatWeb3Frontend/Program.cs
+++ ChatWeb3Frontend/Program.cs
@@ -1,4 +1,5 @@
 using ChatWeb3Frontend;
+using MetaMask.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddMetaMaskBlazor();
 await builder.Build().RunAsync();
