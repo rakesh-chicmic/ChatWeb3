e459df910f3e8b8b7a2076293a1fce04ca5289a6
ajay-chicmic <ajay.joshi@chicmic.co.in>
2023-07-17 15:17:14 +0530

    ApiCalling Service implemented

---
+++ ChatWeb3Frontend/Services/ApiCalling.cs
@@ -0,0 +1,20 @@
+﻿using ChatWeb3Frontend.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace ChatWeb3Frontend.Services
+{
+    public class ApiCalling : IApiCalling
+    {
+        private readonly HttpClient _httpClient;
+        public ApiCalling(HttpClient _httpClient)
+        {
+                this._httpClient = _httpClient;
+        }
+        public Response getYourself()
+        {
+            var loginResult =  _httpClient.GetFromJsonAsync($"{baseUrl}api/v1/login");
+            return new Response { };
+        }
+    }
+}
